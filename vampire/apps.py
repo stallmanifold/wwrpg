91c2d741fec0f943d4a1cd228abc28497f62846a
LambdaXymox <lambda.xymox@gmail.com>
2018-04-19 22:52:38 +0000

    Initialize vampire rule set.

---
+++ vampire/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class VampireConfig(AppConfig):
+    name = 'vampire'
--- wwrpg/settings.py
+++ wwrpg/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'vampire.apps.VampireConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'wwrpg.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'database/db.sqlite3'),
     }
 }
 
